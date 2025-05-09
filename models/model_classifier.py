3daef39a6634efd7b1391220551012e7cc552c0d
if20b141 <115573202+if20b141@users.noreply.github.com>
2025-05-09 16:41:30 +0200

    Update model_classifier.py

--- models/model_classifier.py
+++ models/model_classifier.py
@@ -4,54 +4,28 @@ import torch.nn.functional as F
 
 
 class AudioMLP(nn.Module):
-    def __init__(self, n_steps, n_mels, hidden1_size, hidden2_size, output_size, time_reduce=1):
-        super().__init__()
+    def __init__(self, n_steps, n_mels, hidden1_size, hidden2_size, output_size, time_reduce=1, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.time_reduce = time_reduce
-        self.n_steps = n_steps
-        self.n_mels = n_mels
+        # optimized for GPU, faster than x.reshape(*x.shape[:-1], -1, 2).mean(-1)
+        self.pool = nn.AvgPool1d(kernel_size=time_reduce, stride=time_reduce)  # Non-overlapping averaging
 
-        self.pool = nn.AvgPool1d(kernel_size=time_reduce, stride=time_reduce) if time_reduce > 1 else nn.Identity()
-
-        # Feature extractor
-        self.conv_block = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-
-            nn.Conv2d(16, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.AdaptiveAvgPool2d((1, 1))  # (B, 64, 1, 1)
-        )
-
-        # Fully connected head
-        self.fc = nn.Sequential(
-            nn.Flatten(),  # -> (B, 64)
-            nn.Linear(64, hidden1_size),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(hidden1_size, hidden2_size),
-            nn.ReLU(),
-            nn.Linear(hidden2_size, output_size)
-        )
+        self.fc1 = nn.Linear(n_steps * n_mels, hidden1_size)
+        self.fc2 = nn.Linear(hidden1_size, hidden2_size)
+        self.fc3 = nn.Linear(hidden2_size, output_size)
+        self.dropout = nn.Dropout(0.3)
 
     def forward(self, x):
-        # x shape: (B, n_mels, n_steps)
-        b = x.shape[0]
-        x = x.view(b, 1, self.n_mels, self.n_steps)  # (B, 1, n_mels, time)
+        # reduce time dimension
+        shape = x.shape
+        x = x.reshape(-1, 1, x.shape[-1])
+        x = self.pool(x)  # (4096, 1, 431//n)
+        x = x.reshape(shape[0], shape[1], shape[2], -1)
 
-        if self.time_reduce > 1:
-            x = x.view(b, 1 * self.n_mels, self.n_steps)  # (B, C=1*n_mels, time)
-            x = self.pool(x)
-            new_time = x.shape[-1]
-            x = x.view(b, 1, self.n_mels, new_time)
-
-        x = self.conv_block(x)
-        x = self.fc(x)
+        # 2D to 1D
+        x = nn.Flatten()(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
